10975c11f39c92f5183ad814260993655b895fa1
nevin47 <nevin47@nevin47-MacBook-Pro.local>
2015-05-26 20:25:47 +0800

    change
    change

--- IntervalNumCore/IntervalNum.py
+++ IntervalNumCore/IntervalNum.py
@@ -1,24 +1,27 @@
 import numpy as np
 
-class InterVal:
+class Interval:
     ##Initialise the interval number
     def __init__(self,Lower,Upper):
-        self.Upper = float(Upper)
-        self.Lower = float(Lower)
-        print "Init a new interval number :[",self.Lower,",",self.Upper,"]"
-    
+        if Lower >= Upper:
+            print "Illegle!"
+        else:
+            self.Upper = float(Upper)
+            self.Lower = float(Lower)
+            print "Init a new interval number :[",self.Lower,",",self.Upper,"]"
+        
     ##Set the method of plus
     def IntervalNumPlus(self,b):
         newUpper = self.Upper + b.Upper
         newLower = self.Lower + b.Lower
-        newInterNum = InterVal(newLower,newUpper)
+        newInterNum = Interval(newLower,newUpper)
         return newInterNum
     
     ##Set the method of minus
     def IntervalNumMinus(self,b):
         newUpper = self.Upper - b.Upper
         newLower = self.Lower - b.Lower
-        newInterNum = [newLower,newUpper]
+        newInterNum = Interval(newLower,newUpper)
         return newInterNum
     
     ##Set the method of multiply
@@ -26,13 +29,17 @@ class InterVal:
         testArray = [self.Upper*b.Upper,self.Lower*b.Lower,self.Upper*b.Lower,self.Lower*b.Upper]
         newUpper = max(testArray)
         newLower = min(testArray)
-        newInterNum = [newLower,newUpper]
+        newInterNum = Interval(newLower,newUpper)
         return newInterNum
     
     ##Set the method of devide
-    def IntervalNumDevide(a,b):
-        testArray = [a[0]/b[0],a[1]/b[0],a[1]/b[1],a[0]/b[1]]
+    def IntervalNumDevide(self,b):
+        testArray = [self.Upper/b.Upper,self.Lower/b.Lower,self.Upper/b.Lower,self.Lower/b.Upper]
         newUpper = max(testArray)
         newLower = min(testArray)
-        newInterNum = [newLower,newUpper]
+        newInterNum = Interval(newLower,newUpper)
         return newInterNum
+        
+    ##Set the method of output the Interval Number
+    def IntervalOutput(self):
+        print "This num is: [" , self.Lower , "," , self.Upper , "]"
